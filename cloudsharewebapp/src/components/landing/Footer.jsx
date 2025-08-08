99034f0febdbc4dcc4312952f38881716d2af13a
Vijaypal64328 <singhvijaypal64328@gmail.com>
2025-08-09 03:32:54 +0530

    Add all project files as regular folders

---
+++ cloudsharewebapp/src/components/landing/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+                <div className="text-center">
+                    <p className="text-base text-gray-400">
+                        &copy; 2025 CloudShare. All rights reserved.
+                    </p>
+                    <p className="text-base text-white mt-2">
+                        Developed by  Vijaypal Singh
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
